14c21cbb497193a29c2082f41d0a324f62f50c4f
Vincent Le <vinnyoodles@gmail.com>
2016-11-20 00:25:05 -0800

    Add pascals triangle solution

--- spec/recursion/pascalsTriangleSpec.js
+++ spec/recursion/pascalsTriangleSpec.js
@@ -2,14 +2,39 @@ var pascalsTriangle = require('../../src/recursion/pascalsTriangle');
 
 describe('pascalsTriangle', () => {
   it('should return pascal\'s triangle', () => {
-    expect(pascalsTriangle(1).toEqual(['1']));
+    expect(pascalsTriangle(1)).toEqual([[1]]);
   });
 
   it('should return pascal\'s triangle with 3 levels', () => {
-    expect(pascalsTriangle(3).toEqual(['1', '1 1', '1 2 1']));
+    expect(pascalsTriangle(3)).toEqual([
+      [1],
+      [1, 1],
+      [1, 2, 1]
+    ]);
   });
 
   it('should return pascal\'s triangle with 5 levels', () => {
-    expect(pascalsTriangle(4).toEqual(['1', '1 1', '1 2 1', '1 3 3 1', '1 4 6 4 1']));
+    expect(pascalsTriangle(5)).toEqual([
+      [1],
+      [1, 1],
+      [1, 2, 1],
+      [1, 3, 3, 1],
+      [1, 4, 6, 4, 1]
+    ]);
+  });
+
+  it('should return pascal\'s triangle with 10 levels', () => {
+    expect(pascalsTriangle(10)).toEqual([
+      [1],
+      [1, 1],
+      [1, 2, 1],
+      [1, 3, 3, 1],
+      [1, 4, 6, 4, 1],
+      [1, 5, 10, 10, 5, 1],
+      [1, 6, 15, 20, 15, 6, 1],
+      [1, 7, 21, 35, 35, 21, 7, 1],
+      [1, 8, 28, 56, 70, 56, 28, 8, 1],
+      [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+    ]);
   });
 });
--- src/recursion/pascalsTriangle.js
+++ src/recursion/pascalsTriangle.js
@@ -2,8 +2,30 @@
  * Pascal's triangle is defined as the rth column of the nth row is n! / ( r! x (n - r)!)
  *
  * @param {Integer} n
- * @return {Array} string array where each value represents a level in the triangle.
+ * @return {Array} int array where each value represents a level in the triangle.
  */
 function pascalsTriangle(n) {
+  // Check if n is 0.
+  if (!n) return [];
+
+  // Start with the first level already made.
+  var triangle = [[1]];
+
+  // Add each level after the first.
+  for (var i = 0; i < n - 1; i++) {
+    // Each level starts with 1.
+    var level = [1];
+
+    for (var j = 1; j < triangle[i].length; j++) {
+      level[j] = triangle[i][j] + triangle[i][j - 1];
+    }
+
+    // Each level ends with 1.
+    level.push(1);
+    triangle.push(level);
+  }
+
+  return triangle;
 }
+
 module.exports = pascalsTriangle;
