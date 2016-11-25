387245f5fa7dc2bd0b55ac1ba558daaa4cd894d2
Vincent Le <vinnyoodles@gmail.com>
2016-11-24 20:21:23 -0800

    Add readme for string

--- spec/string/longestSubsequenceSpec.js
+++ spec/string/longestSubsequenceSpec.js
@@ -17,4 +17,40 @@ describe('longestSubsequence', () => {
     var string2 = 'abbcacdbacbaddbcadbcabdcabdc';
     expect(subsequence(string1, string2)).toEqual(9);
   });
+
+  it('should return 0 when no subsequence exists', () => {
+    var string1 = 'abcd';
+    var string2 = 'xyz';
+    expect(subsequence(string1, string2)).toEqual(0);
+  });
+
+  it('should count whitespace', () => {
+    var string1 = 'tttttttttttttttttttt abc wwwwwwwwwwwwwww';
+    var string2 = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz abc zzzzzzzzzzzzzzzzzzz';
+    expect(subsequence(string1, string2)).toEqual(5);
+  });
+
+  it('should match the subsequence found at the end', () => {
+    var string1 = 'qweeeeeeeeeeeeeeeeqweqweqweqweqweqweqweqweqwqweqwqweqweqwabc';
+    var string2 = 'popopopopopioiopioppoipiipoioopipoipoipoipoipoipoiabc';
+    expect(subsequence(string1, string2)).toEqual(3);
+  });
+
+  it('should match the subsequence found at the beginning', () => {
+    var string1 = 'abcqweeeeeeeeeeeeeeeeqweqweqweqweqweqweqweqweqwqweqwqweqweqw';
+    var string2 = 'abcpopopopopopioiopioppoipiipoioopipoipoipoipoipoipoi';
+    expect(subsequence(string1, string2)).toEqual(3);
+  });
+
+  it('should not have to be continuous', () => {
+    var string1 = 'zzzzzzzazzzzzzbzzzzzzc';
+    var string2 = 'tttttabctttt';
+    expect(subsequence(string1, string2)).toEqual(3);
+    var string3 = 'zzzzzzzazzzzzzbzzzzzzc';
+    var string4 = 'ttttatttttbttttc';
+    expect(subsequence(string3, string4)).toEqual(3);
+    var string5 = 'zzzzzzabczzzzz';
+    var string6 = 'ttttatttttbttttc';
+    expect(subsequence(string5, string6)).toEqual(3);
+  });
 });
