8546d1746ed418f2c714dbbdaed196318d0633b8
Vincent Le <vinnyoodles@gmail.com>
2016-12-15 09:55:33 -0800

    Extend ListNode methods in DoublyListNode

--- src/linkedlist/DoublyListNode.js
+++ src/linkedlist/DoublyListNode.js
@@ -1,23 +1,12 @@
+var ListNode = require('./ListNode');
+
 function DoublyListNode(value) {
   this.next = null;
   this.prev = null;
   this.value = value;
 }
 
-/**
- * Return the linked list as an array of each nodes' value.
- * @return {Array}
- */
-DoublyListNode.prototype.toArray = function() {
-  var clone = this;
-  var array = [];
-
-  while (clone) {
-    if (typeof clone.value === 'number') array.push(clone.value);
-    clone = clone.next;
-  }
-
-  return array;
-};
+DoublyListNode.prototype = ListNode.prototype;
+DoublyListNode.prototype.constructor = DoublyListNode;
 
 module.exports = DoublyListNode;
