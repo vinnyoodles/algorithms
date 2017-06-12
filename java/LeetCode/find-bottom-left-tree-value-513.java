c67ebf372e67b3e6200e17f28e923ae39a7ee097
Vincent Le <vinnyoodles@gmail.com>
2017-06-11 20:41:15 -0700

    Add leetcode #513

---
+++ java/LeetCode/find-bottom-left-tree-value-513.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+ *
+ * Solution:
+ * Use bfs to reach every node.
+ * Use different counters/variables to keep track of the first/left most node in every level.
+ * Complexity: O(n) time and space where n is the # of nodes in the tree.
+ */
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        int current = 1;
+        int next = 0;
+        int left = root.val;
+        int childCount = -1;
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (next == 0 && ((current > 1 && current == childCount) || (current == 1 && childCount == 1))) {
+                left = node.val;
+            }
+            current --;
+            
+            if (node.left != null) {
+                queue.add(node.left);
+                next ++;
+            }
+            
+            if (node.right != null) {
+                queue.add(node.right);
+                next ++;
+            }
+            
+            if (current == 0) {
+                current = next;
+                childCount = next;
+                next = 0;
+            }
+        }
+        
+        return left;
+    }
+}
