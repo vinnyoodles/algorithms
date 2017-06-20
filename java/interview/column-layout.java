335e271cff447e59ed177d7a89ff58db7e0c42e3
Vincent Le <vinnyoodles@gmail.com>
2017-06-20 16:13:01 -0700

    Add pinterest problem

---
+++ java/interview/column-layout.java
@@ -0,0 +1,27 @@
+/**
+ * @Company Pinterest
+ *
+ * Given a number of columns and an array of pin objects, return an array containing the optimal height of each column.
+ * A pin object is defined as:
+ *
+ * class Pin {
+ *   int height;
+ * }
+ *
+ * The optimal heights for the column will minimize the height of the largest column.
+ */
+public int[] columnLayout(int k, Pin[] pins) { }
+
+
+/**
+ * Part two will expand on the pin object to be:
+ *
+ * class Pin {
+ *   int height;
+ *   int width;
+ * }
+ *
+ * You are also given the width of the column, so that any pin that does not equal the column's width
+ * must be resized proportionally to fit the column.
+ */
+public int[] columnLayout(int k, int width, Pin[] pins) { }
