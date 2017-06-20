6c191bd67e7598c687b4cbf766cda33dac6e7eb0
Vincent Le <vinnyoodles@gmail.com>
2017-06-20 16:23:10 -0700

    Add riot games #2

---
+++ java/interview/most-active-kill-window.java
@@ -0,0 +1,11 @@
+/**
+ * @Company Riot Games
+ *
+ * Given a list of kills, return the start of the 30 second window that contains the most number of kills.
+ * A kill is defined as an integer array of the format [ timestamp, killerId, victimId ].
+ * If there is a tie, then return an array of all the windows.
+ *
+ * The return format should be a list of all the windows with the most number of kills in the format of an array.
+ * [ timestamp, number of kills ]
+ */
+public int[][] mostActiveKillWindow(int[][] kills) { }
