381e8b191b137c35b96e3fec50de663467d7b443
Vincent Le <vinnyoodles@gmail.com>
2017-06-21 16:46:03 -0700

    Fix comments and styling

--- java/leetcode/longest-substring-without-repeating-characters-3.java
+++ java/leetcode/longest-substring-without-repeating-characters-3.java
@@ -5,27 +5,25 @@
  */
 public class Solution {
   public int lengthOfLongestSubstring(String s) {
-
-int j = 0; // front pointer
-int i = 0; // back pointer
-int size = s.length();
-HashSet<Character> set = new HashSet<Character>();
-int result = 0;
-while ( i < size && j < size) {
-  // adds only the unique values
-  if (!set.contains(s.charAt(i))) {
-    // Move the back pointer, increasing the substring length.
-    i++;
-    set.add(s.charAt(i - 1));
-    // i - j, since that will give use the length of the substring
-    result = Math.max(result, i - j);
-  } else {
-    // Move the front pointer, decreasing the substring length.
-    j++;
-    set.remove(s.charAt(j - 1));
-  }
-}
-return result;
-
+    int j = 0; // front pointer
+    int i = 0; // back pointer
+    int size = s.length();
+    HashSet<Character> set = new HashSet<Character>();
+    int result = 0;
+    while ( i < size && j < size) {
+      // adds only the unique values
+      if (!set.contains(s.charAt(i))) {
+        // Move the back pointer, increasing the substring length.
+        i++;
+        set.add(s.charAt(i - 1));
+        // i - j, since that will give use the length of the substring
+        result = Math.max(result, i - j);
+      } else {
+        // Move the front pointer, decreasing the substring length.
+        j++;
+        set.remove(s.charAt(j - 1));
+      }
+    }
+    return result;
   }
 }
