dc523fca2dcda31082f8f9af8999ef30a8052c55
Vincent Le <vinnyoodles@gmail.com>
2017-06-22 11:57:18 -0700

    Fix param order

--- java/interview/bipartition-pattern-matching.java
+++ java/interview/bipartition-pattern-matching.java
@@ -11,4 +11,33 @@
  *   Input: 'cat dog dog foo', 'abba'
  *   Output: false because cat = a so foo cannot == a
  */
-public boolean isValidPattern(String str, String pattern) { }
+public boolean wordPattern(String str, String pattern) {
+  // map each letter to a string.
+  String[] strs = str.split("\\s");
+  // character -> string mapping
+  String[] map = new String[26];
+  HashSet<String> set = new HashSet<String>();
+
+  // Must be equal in length.
+  if (strs.length != pattern.length()) return false;
+
+  for (int i = 0; i < strs.length; i ++) {
+    String string = strs[i];
+    char c = pattern.charAt(i);
+    int index = c - 'a';
+
+    // First occurence of the character.
+    if (map[index] == null && !set.contains(string)) {
+      map[index] = string;
+      set.add(string);
+
+    // Valid pattern match
+    } else if (map[index] != null && map[index].equals(string)) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
