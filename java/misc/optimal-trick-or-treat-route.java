d515d45078714233d59fcfd243b640e44d0c8829
Puyush Goel <puyushgoel@gmail.com>
2017-07-05 21:51:46 -0400

    fix

--- java/misc/optimal-trick-or-treat-route.java
+++ java/misc/optimal-trick-or-treat-route.java
@@ -39,50 +39,50 @@ public int optimalRoute(int[][] houses) {
   if (rows == 0) {
     return 0;
   }
+  int cols = houses[0].length;
+  int[][] dp = new int[rows][cols];
   if (rows == 1) {
     return maxCandy(dp[rows]);
   }
-  int cols = houses[0].length;
-  int[][] dp = new int[rows][cols];
-  for (int i = 0; i < col; i++) { // copy the first row
+  for (int i = 0; i < cols; i++) { // copy the first row
     dp[0][i] = houses[0][i];
   }
-  helper(houses, dp, rows, cols)
+  helper(houses, dp, rows, cols);
   return maxCandy(dp[rows - 1]); // max candy
 }
 
-private void helper(int[]][] houses, int[][] dp, int rows, int cols) {
+private void helper(int[][] houses, int[][] dp, int rows, int cols) {
   // we are going to add dp[]
   // making sure we can go can access all of the row and not get out of bounds
   for (int i = 0; i < rows - 1; i++) {
     for (int j = 0; j < cols; j++) {
-      if (j > 1 && j < cols) {
-        int left = houses[i][j] + houses[i][j - 1];
+      if (j >= 1 && j < cols - 1) {
+        int left = houses[i][j] + houses[i + 1][j - 1];
         int down = houses[i][j] + houses[i + 1][j];
-        int right = houses[i1][j] + houses[i + 1][j + 1];
+        int right = houses[i][j] + houses[i + 1][j + 1];
         int maxLeft = Math.max(left, dp[i + 1][j - 1]);
-        int maxDown = Math.max(down, dp[i + 2][j]);
-        int maxRight = Math.max(right, dp[i + 2][j + 1]);
+        int maxDown = Math.max(down, dp[i + 1][j]);
+        int maxRight = Math.max(right, dp[i + 1][j + 1]);
         dp[i + 1][j - 1] = maxLeft;
-        dp[i + 2][j] = maxDown;
-        dp[i + 2][j + 1] = maxRight;
+        dp[i + 1][j] = maxDown;
+        dp[i + 1][j + 1] = maxRight;
       } else if (j + 1 > cols && j - 1 > 0){ // when we can only go down and left
         // we have to get the max and then populate the dp array.
         int left = houses[i][j] + houses[i][j - 1];
         int down = houses[i][j] + houses[i + 1][j];
-        int maxLeft = Math.max(left, dp[i + 1][j - 1]);
-        int maxDown = Math.max(down, dp[i + 2][j]);
-        dp[i + 1][j - 1] = maxLeft;
-        dp[i + 2][j] = maxDown;
+        int maxLeft = Math.max(left, dp[i ][j - 1]);
+        int maxDown = Math.max(down, dp[i + 1][j]);
+        dp[i ][j - 1] = maxLeft;
+        dp[i + 1][j] = maxDown;
 
       } else if (j - 1 < 0 && j + 1 < cols) { // when we can go right and down
         // since nothing is populated yet we don't have to worry about the only storing the max value
-        dp[i + 2][j] = houses[i][j] + houses[i + 1][j]; // when we only go down
-        dp[i + 2][j + 1]  = houses[i][j] + houses[i + 1][j + 1]; // we are looking at the right side.
+        dp[i + 1][j] = houses[i][j] + houses[i + 1][j]; // when we only go down
+        dp[i + 1][j + 1]  = houses[i][j] + houses[i + 1][j + 1]; // we are looking at the right side.
       }
     }
     for (int index = 0; index < cols; index++) { // copy the values to our array so we can compute the correct sum
-      houses[i + 1][index] = dp[i + 2][index];
+      houses[i + 1][index] = dp[i + 1][index];
     }
   }
 }
