4798b595e415b7d6e829ca6bfe825c8032f8fdc8
Vincent Le <vinnyoodles@gmail.com>
2017-07-06 19:09:48 -0700

    Add solution for largest killstreak

--- java/interview/largest-killstreak.java
+++ java/interview/largest-killstreak.java
@@ -6,8 +6,7 @@
  * of kills where each kill is no more than 15 units of time from the next kill.
  *
  * If there is a tie of users with the largest killstreak, then return an array of them.
- * The return format should be a list of all the users with the largest killstreak in the format of an array.
- * [ killerId, number of kills ]
+ * The return format should be a list of all the users with the largest killstreak in the format of a list of their ids.
  *
  * Problem Ex.
  *   Input: [
@@ -16,13 +15,38 @@
  *     [ 3, 1, 4 ],
  *     [ 9, 2, 1 ],
  *   ]
- *   Output: [ 
- *     [ 1, 3 ] 
- *   ]
+ *   Output: [ 1, 3 ] 
  *
  * Note: the list of kills are not in any specific order.
  */
-public int[][] largestKillstreak(int[][] kills) { }
+public List<Integer> largestKillstreak(int[][] kills) { 
+  Arrays.sort(kills, (a, b) -> a[0] - b[0]);
+  // Mapping of userId -> [ killCount, killTime ].
+  HashMap<Integer, int[]> map = new HashMap<Integer, int[]>();
+
+  int max = Integer.MIN_VALUE;
+  for (int i = 0; i < kills.length; i ++) {
+    int timestamp = kills[i][0];
+    int killer = kills[i][1];
+    int pair = map.getOrDefault(killer, new int[] { 0, 0 });
+    if (pair[1] + 15 < timestmap) {
+      pair[0] = 0;
+    }
+    pair[0] ++;
+    pair[1] = timestamp;
+    max = Math.max(max, pair[0]);
+    map.put(killer, pair);
+  }
+
+  List<Integer> list = new ArrayList<Integer>();
+
+  for (int i : map.keySet()) {
+    int[] pair = map.get(i);
+    if (pair[0] == max) list.add(i);
+  }
+
+  return list;
+}
 
 /**
  * Part 2 expands on the definition of killstreak.
@@ -31,4 +55,40 @@ public int[][] largestKillstreak(int[][] kills) { }
  * 
  * This is more commonly the actual definition of a multi killstreak found in games.
  */
-public int[][] largestKillstreak(int[][] kills) { }
+public List<Integer> largestKillstreak(int[][] kills) {
+  Arrays.sort(kills, (a, b) -> a[0] - b[0]);
+  // Mapping of userId -> [ killCount, killTime ].
+  HashMap<Integer, int[]> map = new HashMap<Integer, int[]>();
+
+  int max = Integer.MIN_VALUE;
+  for (int i = 0; i < kills.length; i ++) {
+    int timestamp = kills[i][0];
+    int killer = kills[i][1];
+    int victim = kils[i][2];
+
+    // Reset the victim's kill count.
+    if (map.containsKey(victim)) {
+      int[] victimPair = map.get(victim);
+      victimPair[0] = 0;
+      map.put(victim, victimPair);
+    }
+
+    int pair = map.getOrDefault(killer, new int[] { 0, 0 });
+    if (pair[1] + 15 < timestmap) {
+      pair[0] = 0;
+    }
+    pair[0] ++;
+    pair[1] = timestamp;
+    max = Math.max(max, pair[0]);
+    map.put(killer, pair);
+  }
+
+  List<Integer> list = new ArrayList<Integer>();
+
+  for (int i : map.keySet()) {
+    int[] pair = map.get(i);
+    if (pair[0] == max) list.add(i);
+  }
+
+  return list;
+}
