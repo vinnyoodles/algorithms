3c34128dec117065d5507da924b24153f9002c39
Vincent Le <vinnyoodles@gmail.com>
2018-01-24 13:35:13 -0500

    Add sort and insert heap test

--- java/tests/PriorityQueueTest.java
+++ java/tests/PriorityQueueTest.java
@@ -5,6 +5,7 @@ public class PriorityQueueTest {
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < 5; i ++) {
             testRandomSort(10000 + ((int) (Math.random() * 10000)));
+            testRandomSortAndInsert(10000 + ((int) (Math.random() * 10000)));
         }
     }
 
@@ -28,10 +29,36 @@ public class PriorityQueueTest {
         while (!q.isEmpty()) {
             int curr = q.poll();
 
-            if (curr < prev) {
-                throw new Exception("Incorrect poll order");
-            }
+            if (curr < prev) throw new Exception("Incorrect poll order");
             prev = curr;
+        }
+    }
+
+    private static void testRandomSortAndInsert(int count) throws Exception {
+        System.out.printf("\tRandomSortAndInsert (n = %d)\n", count);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i = 0; i < count; i ++) {
+            q.add((int) (Math.random() * Integer.MAX_VALUE));
+        }
+
+        if (q.isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+
+        if (q.size() != count) {
+            throw new Exception("Queue size is incorrect");
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (q.isEmpty()) break;
+            int next = q.poll();
+
+            if (curr > next) throw new Exception("Incorrect poll order");
+
+            if (Math.random() < 0.5) {
+                q.add((int) (Math.random() * Integer.MAX_VALUE));
+            }
         }
     }
 }
